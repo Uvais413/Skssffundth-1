6db2faa1e194ace086cba968985e9ce7be04b867
Uvais413 <muhammeduvaisap786@gmail.com>
2025-06-27 06:41:55 +0530

    ok

--- script.js
+++ script.js
@@ -4,64 +4,62 @@ document.addEventListener('DOMContentLoaded', () => {
     const passcodeInput = document.getElementById('passcode');
     const errorMessage = document.getElementById('errorMessage');
 
-    // --- Dummy Student Data ---
-    // In a real application, this data would come from a secure backend database.
-    // For this example, we're hardcoding it.
+    
     const students = [
         {
             userId: 'Uvais',
             passcode: '123',
             name: 'Uvais',
-            photo: 'uvais.jpg', // Make sure this image is in your project folder
+            photo: 'uvais.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=1987261765&single=true'
         },
         {
             userId: 'sahl',
             passcode: 'harpper1234',
             name: 'MUHAMMED SAHL NV',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=122098699&single=true'
         },
         {
             userId: 'MUHAMMED ANSIL',
             passcode: 'MUHAMMEDANSIL',
             name: 'MUHAMMED ANSIL AN',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=190390898&single=true'
         },
         {
             userId: '162009',
             passcode: '162009',
             name: 'SAYYID MUHAMMED ADHIL',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=431344764&single=true'
         },
         {
             userId: 'Parkkal islu',
             passcode: 'Islu@2928',
             name: 'MUHAMMED ISLAH P',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=761870303&single=true'
         },
         {
             userId: 'Naseeb12',
             passcode: 'Nibunaseeb',
             name: 'MOHAMMED NASEEB P',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=1417302995&single=true'
         },
         {
             userId: 'Abdullah',
             passcode: '808613',
             name: 'ABDYLLAH M S',
-            photo: 'demo passport 2.jpg', // Make sure this image is in your project folder
+            photo: 'demo passport 2.jpg', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=895386043&single=true'
         },
         {
             userId: 'Basil Kuttoor',
             passcode: '250191',
             name: 'MUHAMMED BASIL A',
-            photo: 'basil.JPG', // Make sure this image is in your project folder
+            photo: 'basil.JPG', 
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTUGsCfaU4vJ27cRbF1qzfTTw0y8Z3MDvmp1os7MtzqTM02ggea1CBrXp0272D-S9uEz-fnNEifYfnc/pubhtml?gid=764584542&single=true'
         },
         {
@@ -79,11 +77,11 @@ document.addEventListener('DOMContentLoaded', () => {
             spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=2070671926&single=true'
         },
         {
-            userId: 'ap',
-            passcode: '1sgd2',
-            name: 'Uvais',
-            photo: 'demo passport 2.jpg', 
-            spreadsheetLink: ''
+            userId: 'Csmnabeelckd',
+            passcode: 'qwsdxcop',
+            name: 'MUHAMMED NABEEL CS',
+            photo: 'nabeel.jpg', 
+            spreadsheetLink: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDW-ZwjYoSeQoNDcYGNxJ819EM7qUh9yGYr2-UqX-2aqvC4V5zQen1nULDaDYyCehfgvSZHybaou1L/pubhtml?gid=1474150325&single=true'
         },
         {
             userId: 'ap',
@@ -131,7 +129,7 @@ document.addEventListener('DOMContentLoaded', () => {
        
         
     ];
-    // --- End Dummy Student Data ---
+    
 
     loginForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Prevent default form submission
